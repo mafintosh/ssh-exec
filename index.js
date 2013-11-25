7bc405739c139008598f2a624ecbcad04bf7f3d5
Mathias Buus <mathiasbuus@gmail.com>
2013-11-25 20:38:48 +0100

    using concat-stream for buffering

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 var Connection = require('ssh2');
 var stream = require('stream-wrapper');
+var concat = require('concat-stream');
+var once = require('once');
 var path = require('path');
 var fs = require('fs');
 
 var HOME = process.env.HOME || process.env.USERPROFILE;
+
+var buffer = function(stream, enc, callback) {
+	var result = undefined;
+	var exited = false;
+
+	callback = once(callback);
+	stream.pipe(concat(function(buffer) {
+		if (enc) buffer = buffer.toString(enc);
+		if (exited) return callback(null, buffer);
+		result = buffer;
+	}));
+
+	stream.on('error', callback);
+	stream.on('exit', function(code) {
+		if (code) {
+			var err = new Error('bad exit code');
+			err.code = code
+			return callback(err);
+		}
+		if (result !== undefined) return callback(null, result);
+		exited = true;
+	});
+};
 
 var exec = function(cmd, opts, callback) {
 	if (typeof opts === 'string') {
@@ -23,8 +48,14 @@ var exec = function(cmd, opts, callback) {
 	var onkey = function() {
 		conn.on('ready', function() {
 			conn.exec(cmd, {env:opts.env}, function(err, stream) {
-				if (err) return output.emit('error', err);
+				if (err) {
+					conn.end();
+					output.emit('error', err);
+					return;
+				}
+
 				stream.pipe(output);
+
 				stream.on('exit', function(code) {
 					conn.end();
 					output.emit('exit', code);
@@ -56,24 +87,7 @@ var exec = function(cmd, opts, callback) {
 
 	if (!callback) return output;
 
-	var buffer = [];
-
-	stream.on('data', function(data) {
-		buffer.push(data);
-	});
-	stream.on('exit', function(code) {
-		if (!code) {
-			var buf = buffer.length === 1 ? buffer[0] : Buffer.concat(buffer);
-			if (opts.encoding) buf = buf.toString(opts.encoding);
-			return callback(null, buf);
-		}
-
-		var err = new Error('bad exit code');
-		err.code = code;
-		callback(err);
-	});
-	stream.on('error', callback);
-
+	buffer(output, opts.encoding, callback);
 	return output;
 };
 
