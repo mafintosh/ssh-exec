ddac6da437bcc5a01ab904980f4db25307ee69fc
Mathias Buus <mathiasbuus@gmail.com>
2015-04-09 22:21:02 -0700

    simplify code

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var stream = require('stream-wrapper')
-var Connection = require('ssh2')
+var ssh2 = require('ssh2')
 var fs = require('fs')
 var path = require('path')
+var duplexify = require('duplexify')
 
 var HOME = process.env.HOME || process.env.USERPROFILE
-
-var noop = function () {}
 
 var parse = function (opts) {
   if (typeof opts === 'string') {
@@ -20,30 +18,72 @@ var parse = function (opts) {
   return opts
 }
 
-var isConnected = function (c) {
-  return c && c._sock && c._sock.readyState === 'open'
-}
-
-var connection = function (opts) {
+var exec = function (cmd, opts) {
   opts = parse(opts)
 
-  var c = new Connection()
+  var stream = duplexify()
+  var client = new ssh2.Client()
   var key = opts.key === false ? undefined : opts.key || path.join(HOME, '.ssh', 'id_rsa')
+  var fingerprint
 
-  var connect = function() {
+  var connect = function () {
     if (key && key.toString().toLowerCase().indexOf('encrypted') > -1) key = null
 
-    c.connect({
+    var verifier = function (hash) {
+      fingerprint = hash
+
+      if (!opts.fingerprint) return true
+      if (fingerprint === opts.fingerprint) return true
+
+      client.destroy(new Error('Host could not be verified'))
+      return false
+    }
+
+    client.connect({
       host: opts.host,
       username: opts.user,
       password: opts.password,
       port: opts.port || 22,
       privateKey: key,
-      agent: process.env.SSH_AUTH_SOCK
+      agent: process.env.SSH_AUTH_SOCK,
+      hostHash: 'md5',
+      hostVerifier: verifier
     })
   }
 
-  if (!key || Buffer.isBuffer(key)) {
+  var run = function () {
+    client.exec(cmd, function (err, stdio) {
+      if (err) return stream.destroy(err)
+
+      stream.setWritable(stdio)
+      stream.setReadable(stdio)
+      stdio.stderr.resume()
+
+      stdio.on('close', function () {
+        client.end()
+      })
+
+      stdio.on('exit', function (code) {
+        stream.emit('exit', code)
+      })
+    })
+  }
+
+  var onverify = function (err) {
+    if (err) return stream.destroy(err)
+    run()
+  }
+
+  client.once('ready', function () {
+    if (fingerprint === opts.fingerprint) return run()
+    if (!stream.emit('verify', fingerprint, onverify)) return run()
+  })
+
+  stream.on('close', function () {
+    client.end()
+  })
+
+  if (!key || Buffer.isBuffer(key) || key.toString().indexOf('\n') > -1) {
     connect()
   } else {
     fs.readFile(key, function (_, buffer) {
@@ -52,100 +92,7 @@ var connection = function (opts) {
     })
   }
 
-  return c
+  return stream
 }
-
-var exec = function (cmd, opts) {
-  if (opts instanceof Connection) return exec(cmd, {connection: opts})
-  opts = parse(opts)
-
-  var buffer = null
-  var waiting = noop
-  var c = opts.connection
-
-  var duplex = stream.passThrough(noop, function (data, enc, callback) {
-    buffer = data
-    waiting = callback
-  })
-
-  var onerror = function (err) {
-    duplex.emit('error', err)
-    duplex.destroy()
-  }
-
-  var onconnected = function () {
-    c.exec(cmd, {env: opts.env}, function (err, stdio) {
-      if (err) return onerror(err)
-
-      var drained = true
-      var update = function () {
-        if (!stdio.readable) return
-        if (drained && stdio.paused) stdio.resume()
-        else if (!drained && !stdio.paused) stdio.pause()
-      }
-
-      duplex._read = function () {
-        drained = true
-        process.nextTick(update)
-      }
-
-      duplex._write = function (data, enc, callback) {
-        if (stdio.write(data) !== false) return callback()
-        stdio.once('drain', callback)
-      }
-
-      duplex.on('finish', function () {
-        stdio.end()
-      })
-
-      stdio.on('data', function (data) {
-        if (duplex.push(data)) return
-        drained = false
-        process.nextTick(update)
-      })
-
-      stdio.on('end', function () {
-        duplex.push(null)
-      })
-
-      stdio.on('close', function () {
-        duplex.destroy()
-      })
-
-      stdio.on('exit', function (code) {
-        duplex.emit('exit', code)
-      })
-
-      if (!buffer) return
-
-      duplex._write(buffer, null, waiting)
-      buffer = null
-      waiting = noop
-    })
-  }
-
-  if (isConnected(c)) {
-    onconnected()
-  } else if (c) {
-    c.on('ready', onconnected)
-  } else {
-    c = connection(opts)
-
-    c.on('error', onerror)
-    c.on('ready', onconnected)
-
-    c.on('close', function () {
-      duplex.destroy()
-    })
-
-    duplex.on('close', function () {
-      c.end()
-    })
-  }
-
-  return duplex
-}
-
-exec.connection = connection
 
 module.exports = exec
